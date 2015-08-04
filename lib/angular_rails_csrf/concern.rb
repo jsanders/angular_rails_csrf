9cc63ec9c03647ea5a38bcdff1b5848102dd3ae6
Hemslo Wang <hemslo.wang@gmail.com>
2015-08-04 18:33:20 +0800

    Fix streaming issue

--- lib/angular_rails_csrf/concern.rb
+++ lib/angular_rails_csrf/concern.rb
@@ -3,7 +3,8 @@ module AngularRailsCsrf
     extend ActiveSupport::Concern
 
     included do
-      after_filter :set_xsrf_token_cookie
+      before_filter :set_xsrf_token_cookie
+      after_filter :set_xsrf_token_cookie, unless: -> { response.committed? }
     end
 
     def set_xsrf_token_cookie
